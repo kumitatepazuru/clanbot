e13599d1b3337da93bca35e205023c58d2b18335
kumitatepazuru <riku.ueda@outlook.jp>
2021-05-18 07:14:45 +0900

    fixed bug

--- cogs/vote.py
+++ cogs/vote.py
@@ -3,9 +3,9 @@ import os
 from io import BytesIO
 
 import discord
+from PIL import Image
 from discord.ext import commands
 
-from PIL import Image
 from main import main
 
 
@@ -15,14 +15,14 @@ class vote(commands.Cog):
         self.logger = logging.getLogger(__name__)
 
     @commands.command(aliases=["ss"])
-    async def showservers(self,ctx):
+    async def showservers(self, ctx):
         if os.path.isfile("./srt/screenshot.jpg"):
             im = Image.open("./srt/screenshot.jpg")
-            im = im.crop((341,217,681,322))
+            im = im.crop((341, 280, 681, 327))
             img_bytes = BytesIO()
-            im.save(img_bytes,format="PNG")
+            im.save(img_bytes, format="PNG")
             img_bytes.seek(0)
-            await ctx.send("今のANNIサーバー一覧",file=discord.File(img_bytes,filename="screenshot.jpg"))
+            await ctx.send("今のANNIサーバー一覧", file=discord.File(img_bytes, filename="screenshot.jpg"))
         else:
             await ctx.send("申しわかりません。ただいまANNIサーバー検知システムを起動中(再起動中)です。もうしばらく(最大10分)お待ちください。")
 
