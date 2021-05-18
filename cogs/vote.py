3550c3dc0456b02847d9ee3b36e4b5fd1ffcb8da
kumitatepazuru <riku.ueda@outlook.jp>
2021-05-18 16:39:16 +0900

    text機能追加

--- cogs/vote.py
+++ cogs/vote.py
@@ -3,6 +3,7 @@ import os
 from io import BytesIO
 
 import discord
+import numpy as np
 from PIL import Image
 from discord.ext import commands
 
@@ -22,7 +23,19 @@ class vote(commands.Cog):
             img_bytes = BytesIO()
             im.save(img_bytes, format="PNG")
             img_bytes.seek(0)
-            await ctx.send("今のANNIサーバー一覧", file=discord.File(img_bytes, filename="screenshot.jpg"))
+            await ctx.send("今のANNIサーバー一覧\n\n**表示の見方**\n緑: 参加できます\n青: phase 3です。プレミアムの方は参加できます\n赤: phase 4以上で参加できません。", file=discord.File(img_bytes, filename="screenshot.jpg"))
+        else:
+            await ctx.send("申しわかりません。ただいまANNIサーバー検知システムを起動中(再起動中)です。もうしばらく(最大10分)お待ちください。")
+
+    @commands.command(aliases=["ts"])
+    async def textservers(self,ctx):
+        if os.path.isfile("./srt/screenshot.jpg"):
+            im = np.array(Image.open("./srt/screenshot.jpg"))
+            l = []
+            for i in range(9):
+                p = im[363+i*36,295]
+                l.append(np.argmax(p))
+            await ctx.send(" ".join(l))
         else:
             await ctx.send("申しわかりません。ただいまANNIサーバー検知システムを起動中(再起動中)です。もうしばらく(最大10分)お待ちください。")
 
